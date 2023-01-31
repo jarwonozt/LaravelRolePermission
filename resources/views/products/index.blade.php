c64bb70aadfdd2a885643c1ea7a7ec876432f029
jarwonozt <jarwonoztaveiro@gmail.com>
2023-01-31 21:48:54 +0700

    update table

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -41,7 +41,7 @@
                 <form action="{{ route('products.destroy',$product->id) }}" method="POST">
                     {{-- <a class="btn btn-info" href="{{ route('products.show',$product->id) }}">Show</a> --}}
                     @can('product-edit')
-                    <a class="btn btn-{{ $product->status == true ? 'success' : 'warning' }}" href="{{ route('products.edit',$product->id) }}">{{ $product->status == true ? 'Nonakfikan' : 'Aktifkan' }}</a>
+                    <a class="btn btn-sm btn-{{ $product->status == true ? 'success' : 'warning' }}" href="{{ route('products.edit',$product->id) }}">{{ $product->status == true ? 'Nonakfikan' : 'Aktifkan' }}</a>
                     @endcan
 
 
